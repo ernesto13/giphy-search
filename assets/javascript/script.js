1014699313f1686ab3e22237b62e9ef70eb18d04
ernstsamaniego <ernstsamaniego@yahoo.com>
2019-07-06 19:00:47 -0400

    added for loop to display more giphs

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,20 +5,26 @@ $(document).ready(() => {
     var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=" + giph + "&api_key=" + API + "&limit=5");
     xhr.done(function(data) {
       //       Access-Control-Allow-Origin: *;
-      let image = $("<img>").attr("src", data.data[1].images.original.url);
+
+      let giphySearch = data.data;
+      for (let i = 0; i < giphySearch.length; i++) {
+        console.log('loop is ' + giphySearch[i].images.original.url);
+        let image = $("<img class='mb-2'>").attr("src", giphySearch[i].images.original.url);
 
 
-      let giphDiv = $("<div class='giphs mx-auto'>");
-      giphDiv.append(image);
-      $(".images").prepend(giphDiv);
+        let giphDiv = $("<div class='giphs'>");
+        giphDiv.append(image);
+        $(".images").prepend(giphDiv);
 
-      console.log("success got data", data);
+        console.log("success got data", data);
 
-      console.log('my data for gilphs', data.data[1].images.original.url);
+
+
+      }
+
     });
 
   }
-  gilphSearch();
 
   let searchGiph = $('#searchGiph');
 
